da2b38a15b4279798b8a77f5484c103f59ab846b
Henry Roseman <HenBenRo@gmail.com>
2015-07-21 13:56:54 -0700

    R2dance

--- R2-D2 Dance.js
+++ R2-D2 Dance.js
@@ -1,4 +1,5 @@
 var Controller = require('tessel-summer-camp')
+new Controller.Button("G3", function() {
 
 var Servo = new Controller.Servo(1, 2, function() {
   Servo.Move('left', 25)
@@ -22,7 +23,12 @@ var Servo = new Controller.Servo(1, 2, function() {
   Servo.Move('right', 25)
 }, 12000)
   setTimeout(function() {
-    Servo.Move('right', 30)
-    Servo.Move('left', 30)
+    Servo.Move('right', 25)
+    Servo.Move('left', -25)
   }, 15000)
+  setTimeout(function() {
+    Servo.Move('right', 0)
+    Servo.Move('left', 0)
+  })
 })
+});
