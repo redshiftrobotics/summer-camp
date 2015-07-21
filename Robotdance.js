fbe4b09843897531bdfcc6936742d24a7b38f21a
maxr0x <max@maxconnolly.com>
2015-07-21 13:04:47 -0700

    tweeked the button

--- Robotdance.js
+++ Robotdance.js
@@ -1,6 +1,8 @@
 var Controller = require('tessel-summer-camp')
 
 var Servo = new Controller.Servo(1, 2, function() {
+  new Controller.Button("G3", function() {
+      console.log("Pressed!");
  Servo.Move('left', 100)
  Servo.Move('right', -100)
 
@@ -8,4 +10,5 @@ setTimeout(function() {
  Servo.Move('left', -100)
  Servo.Move('right', -100)
  }, 5000)
+ });
 })
