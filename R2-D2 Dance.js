5c3a1ddd0a6386e7e9b47244ad309434ef4eec59
Henry Roseman <HenBenRo@gmail.com>
2015-07-21 13:12:29 -0700

    R2dance

--- R2-D2 Dance.js
+++ R2-D2 Dance.js
@@ -1,28 +1,28 @@
 var Controller = require('tessel-summer-camp')
 
 var Servo = new Controller.Servo(1, 2, function() {
-  Servo.move('left', 25)
-  Servo.move('right', -25)
+  Servo.Move('left', 25)
+  Servo.Move('right', -25)
   setTimeout(function() {
 
-  Servo.move('left',-25)
-  Servo.move('right', 25)
+  Servo.Move('left',-25)
+  Servo.Move('right', 25)
 }, 3000)
   setTimeout(function() {
-    Servo.move('right', 30)
-    Servo.move('left', 30)
+    Servo.Move('right', 30)
+    Servo.Move('left', 30)
   }, 6000)
   setTimeout(function() {
-    Servo.move('right', -30)
-    Servo.move('left', -30)
+    Servo.Move('right', -30)
+    Servo.Move('left', -30)
   }, 9000)
   setTimeout(function() {
 //comment
-  Servo.move('left',-25)
-  Servo.move('right', 25)
+  Servo.Move('left',-25)
+  Servo.Move('right', 25)
 }, 12000)
   setTimeout(function() {
-    Servo.move('right', 30)
-    Servo.move('left', 30)
+    Servo.Move('right', 30)
+    Servo.Move('left', 30)
   }, 15000)
 })
