83c7dc5fdfeb56d6dafe856c21bb46f3bfe5bbf4
maxr0x <max@maxconnolly.com>
2015-07-21 13:32:46 -0700

    We made his dance longer

--- Robotdance.js
+++ Robotdance.js
@@ -1,8 +1,8 @@
 var Controller = require('tessel-summer-camp')
 
 var Servo = new Controller.Servo(1, 2, function() {
-  new Controller.Button("G3", function() {
-      console.log("Pressed!");
+//  new Controller.Button("G3", function() {
+    //  console.log("Pressed!");
  Servo.Move('left', 100)
  Servo.Move('right', -100)
 
@@ -10,5 +10,20 @@ setTimeout(function() {
  Servo.Move('left', -100)
  Servo.Move('right', -100)
  }, 5000)
- });
+// });
+setTimeout(function() {
+ Servo.Move('left', 50)
+ Servo.Move('right', 50)
+ }, 8000
+
+ setTimeout(function() {
+  Servo.Move('left', 0)
+  Servo.Move('right', 0)
+}, 3000
+
+setTimeout(function() {
+  Servo.Move('left', -100)
+  Servo.Move('right', 100)
+}, 10000
+
 })
